84d00a13a0632fa205d10a1cf574bca7e62f2915
Josselin MONTEIL <josselin.monteil@epita.fr>
2023-10-11 17:10:41 +0200

    :recycle: Basic app in sveltkit

---
+++ frontend/src/lib/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/logo-SIGL.png';
+	import team from '$lib/images/logo-200cc.png';
+</script>
+
+<header>
+	<div class="logo-left">
+		<img src={logo} alt="SIGL"/>
+	</div>
+	<h1>Quotes App by 200cc</h1>
+	<div class="logo-right">
+		<img src={team} alt="200cc"/>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 10px;
+		background-color: #333;
+		color: #fff;
+	}
+
+	.logo-left, .logo-right {
+		margin: 0 10px;
+	}
+
+	.logo-left img, .logo-right img {
+		width: 100px;
+		height: 100px;
+	}
+</style>
